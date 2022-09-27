bffd9ab47b9d8b90cb5ef60cd22851e3fd3bc1a7
g0m3s <gom3s.g@gmail.com>
2022-09-27 17:31:31 -0300

    feat: add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,30 @@
 import './index.css'
 import App from './App'
-import React from 'react'
-import './assets/TTCommons/FontFace.css'
+import { Theme } from './utils/theme'
 import ReactDOM from 'react-dom/client'
+import './assets/TTCommons/FontFace.css'
 import { initializeApp } from 'firebase/app'
 import reportWebVitals from './reportWebVitals'
 import { ThemeProvider } from '@mui/material'
-import { Theme } from './utils/theme'
+import { getAnalytics } from 'firebase/analytics'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxCi9qXzLB6xy8v8V1xzxGIyKWYcZgqEE',
-  authDomain: 'conversation-190e6.firebaseapp.com',
-  projectId: 'conversation-190e6',
-  storageBucket: 'conversation-190e6.appspot.com',
-  messagingSenderId: '97203473203',
-  appId: '1:97203473203:web:8b9fbeaa2c23be748a8c46'
+  apiKey: "AIzaSyAxCi9qXzLB6xy8v8V1xzxGIyKWYcZgqEE",
+  authDomain: "conversation-190e6.firebaseapp.com",
+  projectId: "conversation-190e6",
+  storageBucket: "conversation-190e6.appspot.com",
+  messagingSenderId: "97203473203",
+  appId: "1:97203473203:web:8b9fbeaa2c23be748a8c46",
+  measurementId: "G-W7MFDKHTPX"
 }
 
-initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+
+getAnalytics(app)
 
 root.render(
   // <React.StrictMode>
